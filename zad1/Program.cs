839545dddfde76d6ff09870934c946abed55f772
Lodre6123 <nikpash6123@mail.ru>
2023-02-19 18:20:16 +0300

    vodpol

---
+++ zad1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+int NumPolog(int number)
+{
+    int result = 0;
+    for(int i = 0; i < number; i++)
+    {
+        Console.Write($"вы ввели #{i + 1}: ");
+        if(Convert.ToInt32(Console.ReadLine()) > 0)
+            result++;
+    }
+    return result;
+}
+
+Console.Write("Вводим кол-во чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введено {NumPolog(num)} положительных числа");
